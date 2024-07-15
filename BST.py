2d3fc40aa798264c845920283e26351048ff0286
devsojitra37 <devanshsojitra37@gmail.com>
2024-07-15 22:49:45 +0530

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    elif root.val < key:
+        return search(root.right, key)
+    else:
+        return search(root.left, key)
+
+# create a root node
+root = Node(50)
+
+# insert nodes
+root = insert(root, 30)
+root = insert(root, 20)
+root = insert(root, 40)
+root = insert(root, 70)
+root = insert(root, 60)
+root = insert(root, 80)
+
+# search for a node
+result = search(root, 60)
+
+if result is not None:
+    print("Element found")
+else:
+    print("Element not found")
